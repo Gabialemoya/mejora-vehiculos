4f37872fd8be4fe4752d50edb180cdcb29a1a0a9
gabialemoya <gmgabiale72@gmail.com>
2020-10-10 16:05:41 -0300

    Cambios en archivo index.js

--- js/index.js
+++ js/index.js
@@ -1,54 +1,14 @@
 $(document).ready(function(){
+  var i = 0;
 
-    $("#vehiculo").click(function(){
-        
-      var auto = prompt('Ingresar vehículo');
-      
-      var autos = 0;
-      var acumuladorAños = 0;
-
-      
-
-  while(auto){
-    autos++;
-    $("body").append("<p>"+auto+"</p>");
-    var años = parseInt(prompt('Ingresar antigüedad'));
-    acumuladorAños += años;
-    if(pagaPatente(años, 10))
-    {
-      alert('Paga patente');
-    }
-    else{
-      alert('No paga patente');
-    }
-    
-    auto = prompt('Ingresar vehículo');
-  }
-  if(autos>0){
-    var promedio = acumuladorAños/autos;
-    $("body").append("<div><h2>Promedio total: "+promedio+"</h2></div>");
-  }
-  
-  mostrarPromedioVehiculos(autos, acumuladorAños);
+  $('#boton-agregar').on('click',function(){
+    console.log("boton agregar");
+    i++;
+    $('#contador').append('<p>'+i+'</p>');
   });
- 
-  function pagaPatente(antiguedad, limiteAños){
-	return antiguedad < limiteAños;
-}
 
-function mostrarPromedioVehiculos(cantidadAutos, añosTotales){
-	if(cantidadAutos > 0){
-  	resultado = añosTotales / cantidadAutos;
-    alert('El promedio de antigüedad es: ' + resultado);
-  }
-	else{
-  	alert('No se ingresaron vehículos');
-  }
-}
+  $('#agregar').click(function(){
+    $('body').append('<p>Hola</p>');
+  });
 
 });
-
-
-
-
-
