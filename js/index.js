861ce6559d0d6c656ac8bd65012eb3b9a6eff2e0
gabialemoya <gmgabiale72@gmail.com>
2020-10-11 20:20:00 -0300

    Funciona contador

--- js/index.js
+++ js/index.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
   var i = 0;
-
+  var acum = 0;
+  const miArray = [];
   $('#boton-agregar').on('click',function(){
     console.log("boton agregar");
     i++;
-    $('#contador').append('<p>'+i+'</p>');
+    $('#contador').html('<p>Cantidad de vehículos: '+i+'</p>');
+
+    $('entradaAntiguedad').each(function(){
+      miArray.push(this);
+      console.log();
+    })
+   
+    
+  });
+  
+  $('#promedio').on('click',function(){
+    var suma =0;
+    var resultado;
+    for( i=0;i<miArray.length;i++){
+      suma= suma+miArray[i];
+    }
+    
+    resultado= suma/miArray.length;
+    $('#contador').append('<p>Promedio total: '+resultado+'</p>');
+    
   });
 
-  $('#agregar').click(function(){
-    $('body').append('<p>Hola</p>');
-  });
 
 });
+
